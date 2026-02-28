a41f2f1f13e57519ff5ad5bbbc94eb0cff615b2c
JAKE <jake.dev.official@gmail.com>
2026-02-28 10:09:00 +0000

    Hosting Base Changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/", // This is for github remove if on different hosting
   plugins: [react()],
-})
+});
